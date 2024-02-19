99d77610cb51d6559771130022a5e7acb375b540
Thomas CILES <tciles@umanit.fr>
2024-02-19 13:38:46 +0100

    Ajout commande cert generator and idp metadata refresh

--- src/Service/IdpMetadataServiceInterface.php
+++ src/Service/IdpMetadataServiceInterface.php
@@ -8,6 +8,8 @@ use LightSaml\Model\Metadata\EntityDescriptor;
 
 interface IdpMetadataServiceInterface
 {
+    public const DEFAULT_METADATA_CACHE_DURATION = 3600;
+
     public function getEntityDescriptor(string $provider): EntityDescriptor;
 
     public function clearCache(string $provider): void;
