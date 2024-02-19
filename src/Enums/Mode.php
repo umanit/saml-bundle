9d8affcb93e14a18e4b92b67b1d87e48a8bab426
Thomas CILES <tciles@umanit.fr>
2024-02-19 15:57:35 +0100

    Modification de la configuration

---
+++ src/Enums/Mode.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umanit\SamlBundle\Enums;
+
+enum Mode : string
+{
+    case SP_INITIATED = 'sp_initiated';
+    case IDP_INITIATED = 'idp_initiated';
+}
