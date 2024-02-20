48a65e00b3ca9c141fa01459bedae30072adb9e3
Thomas CILES <tciles@umanit.fr>
2024-02-20 14:33:02 +0100

    mise à jour des crt+key

--- src/Controller/AcsAction.php
+++ src/Controller/AcsAction.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Umanit\SamlBundle\Controller;
 
+use LightSaml\Binding\BindingFactory;
+use LightSaml\Context\Profile\MessageContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Umanit\SamlBundle\Service\SpMetadataServiceInterface;
 
@@ -13,10 +16,17 @@ class AcsAction extends AbstractController
 {
     public function __invoke(
         string $provider,
+        Request $request,
         SpMetadataServiceInterface $spMetadataService
     ) {
 
-        dd($spMetadataService->getEntityDescriptor($provider));
+        $messageContext = new MessageContext();
+        $bindingFactory = new BindingFactory();
+        $bindingType = $bindingFactory->detectBindingType($request);
+        $bindingFactory->create($bindingType)->receive($request, $messageContext);
+        $messageContext->setBindingType($bindingType);
+
+        dd($messageContext->getMessage());
 
         return $this->json([]);
     }
