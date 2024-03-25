f75558fc4c7be459748dc55a46f2825f31b9f2b1
ngandon <ngandon@umanit.fr>
2024-03-25 08:20:18 +0100

    feat: test de la response

--- src/Controller/SloAction.php
+++ src/Controller/SloAction.php
@@ -4,19 +4,49 @@ declare(strict_types=1);
 
 namespace Umanit\SamlBundle\Controller;
 
+use LightSaml\Binding\BindingFactory;
+use LightSaml\Context\Profile\MessageContext;
+use LightSaml\Model\Protocol\LogoutResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Umanit\SamlBundle\Service\MetadataServiceInterface;
+use Umanit\SamlBundle\Service\ResponseServiceInterface;
+use Umanit\SamlBundle\Service\SpMetadataServiceInterface;
 
 #[Route('slo/{provider<\w+>}', name: 'umanit_saml_slo', methods: ['GET', 'POST'])]
 class SloAction extends AbstractController
 {
     public function __invoke(
         string $provider,
-        MetadataServiceInterface $metadata
+        SpMetadataServiceInterface $spMetadataService,
+        ResponseServiceInterface $responseService,
+        Request $request,
     ) {
-        dd($metadata->getEntityDescriptor($provider));
+        $response = $this->getSamlMessage($request);
+
+        dd($response);
 
         return $this->json([]);
+    }
+
+    /**
+     * TODO-NGA Cette méthode fonctionne. Voir comment factoriser la function getSamlMessage du response service
+     * qui fait la même chose mais pour autre chose
+     */
+    private function getSamlMessage(Request $request): ?LogoutResponse
+    {
+        $messageContext = new MessageContext();
+        $bindingFactory = new BindingFactory();
+        $bindingType = $bindingFactory->detectBindingType($request);
+        $bindingFactory->create($bindingType)->receive($request, $messageContext);
+        $messageContext->setBindingType($bindingType);
+
+        $response = $messageContext->asLogoutResponse();
+
+        if (!$response instanceof LogoutResponse) {
+            return null;
+        }
+
+        return $response;
     }
 }
