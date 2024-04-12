56d2f02df6e69fe8c8c00171964fd25cb7fa42aa
Thomas CILES <tciles@umanit.fr>
2024-04-12 17:40:38 +0200

    Add configuration

---
+++ src/DependencyInjection/Security/UserProvider/SamlEntityUserProviderFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umanit\SamlBundle\DependencyInjection\Security\UserProvider;
+
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\UserProvider\UserProviderFactoryInterface;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class SamlEntityUserProviderFactory implements UserProviderFactoryInterface
+{
+    public function create(ContainerBuilder $container, string $id, array $config): void
+    {
+        $container
+            ->setDefinition($id, new ChildDefinition('umanit_saml.security.user.saml_entity_user_provider'))
+            ->addArgument($config['user_class'])
+            ->addArgument($config['default_roles'])
+            ->addArgument($config['class'])
+            ->addArgument($config['property'])
+            ->addArgument($config['manager_name'])
+        ;
+    }
+
+    public function getKey(): string
+    {
+        return 'saml_entity';
+    }
+
+    /**
+     * @suppress PhanUndeclaredMethod
+     */
+    public function addConfiguration(NodeDefinition $builder): void
+    {
+        // @formatter:off
+        /** @phpstan-ignore-next-line */
+        $builder
+            ->children()
+                ->scalarNode('user_class')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->validate()
+                        ->ifTrue(static fn ($value) => !is_a($value, UserInterface::class, true))
+                        ->thenInvalid('You should provide user class implementing ' . UserInterface::class . ' interface.')
+                    ->end()
+                ->end()
+                ->arrayNode('default_roles')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(['ROLE_USER'])
+                ->end()
+                ->scalarNode('property')->defaultNull()->end()
+                ->scalarNode('manager_name')->defaultNull()->end()
+            ->end()
+        ;
+        // @formatter:on
+    }
+}
