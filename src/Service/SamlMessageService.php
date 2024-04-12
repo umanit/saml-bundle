34f488959f514261b4f677b50203010001ffc86f
ngandon <ngandon@umanit.fr>
2024-04-12 11:00:01 +0200

    feat: suppression validation du samlMassageService

--- src/Service/SamlMessageService.php
+++ src/Service/SamlMessageService.php
@@ -5,17 +5,10 @@ namespace Umanit\SamlBundle\Service;
 
 use LightSaml\Binding\BindingFactory;
 use LightSaml\Context\Profile\MessageContext;
-use LightSaml\Model\Protocol\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Umanit\SamlBundle\Validator\ResponseValidatorInterface;
 
 class SamlMessageService implements SamlMessageServiceInterface
 {
-    public function __construct(
-        protected ResponseValidatorInterface $responseValidator
-    ) {
-    }
-
     public function getSamlMessage(Request $request): MessageContext
     {
         $messageContext = new MessageContext();
@@ -25,10 +18,5 @@ class SamlMessageService implements SamlMessageServiceInterface
         $messageContext->setBindingType($bindingType);
 
         return $messageContext;
-    }
-
-    public function validate(string $provider, Response $samlMessage, bool $strict = true): void
-    {
-        $this->responseValidator->validate($provider, $samlMessage, $strict);
     }
 }
--- src/Service/SamlMessageServiceInterface.php
+++ src/Service/SamlMessageServiceInterface.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 namespace Umanit\SamlBundle\Service;
 
 use LightSaml\Context\Profile\MessageContext;
-use LightSaml\Model\Protocol\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 interface SamlMessageServiceInterface
 {
     public function getSamlMessage(Request $request): MessageContext;
-
-    public function validate(string $provider, Response $samlMessage, bool $strict = true): void;
 }
