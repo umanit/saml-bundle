6e2430b17b1d7c94fa153d96d6e41dd107e2db69
Thomas CILES <tciles@umanit.fr>
2024-04-29 15:30:40 +0200

    feat: optimize controller

--- src/Controller/MetadataAction.php
+++ src/Controller/MetadataAction.php
@@ -4,34 +4,17 @@ declare(strict_types=1);
 
 namespace Umanit\SamlBundle\Controller;
 
-use Psr\Container\ContainerInterface;
+use LightSaml\SamlConstants;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Service\Attribute\Required;
-use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Umanit\SamlBundle\Guesser\BestContentTypeGuesserInterface;
 use Umanit\SamlBundle\Serializer\SamlElementSerializerInterface;
 use Umanit\SamlBundle\Service\MetadataServiceInterface;
 
-class MetadataAction implements ServiceSubscriberInterface
+class MetadataAction extends AbstractController
 {
-    protected ContainerInterface $container;
-
-    #[Required]
-    public function setContainer(ContainerInterface $container): ?ContainerInterface
-    {
-        $previous = $this->container ?? null;
-        $this->container = $container;
-
-        return $previous;
-    }
-
-    public static function getSubscribedServices(): array
-    {
-        return [];
-    }
-
     #[Route('metadata/{provider<\w+>}', name: 'umanit_saml_metadata')]
     public function __invoke(
         string $provider,
